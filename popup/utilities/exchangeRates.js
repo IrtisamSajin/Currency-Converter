0050cd86744c5beafd097238eafe1af4e732b652
Irtisam Sajin <irtisamcoding@gmail.com>
2023-05-26 19:50:45 +0600

    Version 1.0

--- popup/utilities/exchangeRates.js
+++ popup/utilities/exchangeRates.js
@@ -1,6 +1,6 @@
 import apiUrl from "./api.js"; //Need to have an api key. Here api key is stored in a hidden file. 
 
-const secondsForRetrivingData = 3*24*3600; //The api call will be made once in every 3 days
+const secondsForRetrivingData = 1//3*24*3600; //The api call will be made once in every 3 days
 
 async function setData() {
     let footer = document.getElementById("footer")
@@ -11,7 +11,6 @@ async function setData() {
     try {
         const response = await fetch(apiUrl);
         const jsonData = await response.json();
-        console.log(jsonData)
         let currentTime = Math.floor(Date.now() / 1000);
         chrome.storage.local.set({ lastApiCall: currentTime }, () => {
             console.log(`Time for lastApiCall stored: ${currentTime}`)
